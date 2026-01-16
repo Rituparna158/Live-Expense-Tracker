2d9f9abfb606a5289d37fc4a7841cb0521858cc1
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-01-15 22:13:44 -0800

    feat:add HTML structure

---
+++ script.js
@@ -0,0 +1,7 @@
+const expensename=document.getElementById("expense-name")
+const expenseamount=document.getElementById("expense-amount")
+const form = document.querySelector("form")
+
+form.onsubmit=function(e){
+    e.preventDefault()
+}
